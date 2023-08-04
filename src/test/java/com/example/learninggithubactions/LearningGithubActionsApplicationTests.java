6e3cffaad3cd52ab6cf1e54146ee48d682bf4888
Austyn Harriman <aeharrima@gmail.com>
2023-08-04 10:45:11 -0500

    first

---
+++ src/test/java/com/example/learninggithubactions/LearningGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learninggithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
